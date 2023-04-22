9cbbc3dcd5b2c15acf1df3ecec1d9476bad25f69
pixelstorm <dan@pixelstorm.com.au>
2023-04-22 08:54:32 +0800

    simple greeting object instead of string primitive

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,19 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+const welcome = {
+
+   greeting: 'hey',
+   title: 'reactive'
+
+}
+
+
   return (
       <div>
-        <h1>Hello lika</h1>
-     <label htmlFor="search">Search:</label>
-     <input type="text" id="search"/>
+        <h1>{welcome.greeting} {welcome.title}</h1>
+        <label htmlFor="search">Search:</label>
+        <input type="text" id="search"/>
       </div>
   )
 }
